a24b21f97f511bf543f3366fd0037c2e53e916ee
Alberto Cardoso <albertocardoso9185@gmail.com>
2025-03-19 00:32:31 -0300

    Versão final, Fundamento de colisão e movimento

--- javaScript.js
+++ javaScript.js
@@ -1,19 +1,18 @@
 //obter elemento canvas e se contexto 2d
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getcontext("2");
+const canvas = document.getElementById('meuCanva');
+const ctx = canvas.getContext('2d');
 
-//definir as dimensões do canvas
-const quadradoTamanho = 50;
+//tamanho do quadrado
+const square = 50;
 
-//posição inicial (centralizada)
-let posX = (canvas.width - quadradoTamanho) / 2;
-let PosY = (canvas.height - quadradoTamanho) /2;
+//posição do quadrado
+let posX = (canvas.width - square) / 2;
+let posY = (canvas.height - square) / 2;
 
-//velocidade de movimento
-const velocidade = 5;
+//velocidade do quadrado 
+const speed = 5;
 
-//teclas Pressionadas
-
+// implementação das teclas
 const teclas = {
     ArrowUp: false,
     ArrowDown: false,
@@ -21,10 +20,45 @@ const teclas = {
     ArrowRight: false
 };
 
-//Resistrar eventos de teclado
-window.addEventListener('keydown', function(e){
-    if(teclas.hasOwnProperty(e.key)){
+
+//adicionar eventos de teclado "apertar tecla"
+window.addEventListener('keydown', (e) => {
+    if (teclas.hasOwnProperty(e.key)) {
         teclas[e.key] = true;
-        e.preventeDefault();
+        e.preventDefault();
     }
-})
+});
+
+//adicionar evento de teclado "soltar tecla"
+window.addEventListener('keyup', (e) => {
+    if (teclas.hasOwnProperty(e.key)) {
+        teclas[e.key] = false;
+        e.preventDefault();
+    }
+});
+
+//função de atualização
+function atualizar() {
+
+    //limpa canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //mover quadrado
+
+    if (teclas.ArrowUp && posY > 0) posY -= speed;
+    if (teclas.ArrowDown && posY < canvas.height - square) posY += speed;
+    if (teclas.ArrowLeft && posX > 0) posX -= speed;
+    if (teclas.ArrowRight && posX < canvas.width - square) posX += speed;
+
+
+
+    // Desenha o quadrado
+    ctx.fillStyle = 'red';
+    ctx.fillRect(posX, posY, square, square);
+
+    // Continua o loop de animação
+    requestAnimationFrame(atualizar);
+}
+
+// Inicia a animação
+atualizar();
